ad26c9f0096839b2195b195c1c205d3cca170053
barneygravel <abhinavthegreat@gmail.com>
2020-04-25 20:55:49 +0000

    Initial commit

---
+++ Main.java
@@ -0,0 +1,30 @@
+class Main {
+  public static void main(String[] args) {
+       String out1 = Solve(6);
+       System.out.println(out1);
+       String out2 = Solve(5);
+       System.out.println(out2);
+       String out3 = Solve(28);
+       System.out.println(out3);
+    }
+
+     static String Solve(int N){
+        String result = "NO";
+        
+        if(SumOfFactors(N) == N)
+            result = "YES";
+
+        return result;
+    }
+
+    static int SumOfFactors(int N){
+      int sum = 0;
+      for(int i=1; i<N ; i++)
+        {
+            if(N % i == 0)
+              sum = sum + i;   
+        }
+        return sum;
+    }
+}
+
