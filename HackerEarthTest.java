948c785234f0f8b32c08c4486e7bd4a9bc9719b4
barneygravel <abhinavthegreat@gmail.com>
2020-04-25 21:02:19 +0000

    Separated Main Method.

--- HackerEarthTest.java
+++ HackerEarthTest.java
@@ -1,14 +1,14 @@
 class HackerEarthTest {
-  public static void main(String[] args) {
+  /*public static void main(String[] args) {
        String out1 = Solve(6);
        System.out.println(out1);
        String out2 = Solve(5);
        System.out.println(out2);
        String out3 = Solve(28);
        System.out.println(out3);
-    }
+    }*/
 
-     static String Solve(int N){
+    String Solve(int N){
         String result = "NO";
         
         if(SumOfFactors(N) == N)
@@ -17,7 +17,7 @@ class HackerEarthTest {
         return result;
     }
 
-    static int SumOfFactors(int N){
+    int SumOfFactors(int N){
       int sum = 0;
       for(int i=1; i<N ; i++)
         {
